0881f025c6191caa5417f950e77f610c7ca7b81d
Shashwat Dev <shashwat.dev.iiit.bhubaneswar@gmail.com>
2022-05-20 15:50:18 +0530

    initial commit

---
+++ Recursion/Fibonacci Series.cpp
@@ -0,0 +1,14 @@
+#include<iostream.h>
+using namespace std;
+int fibbo(int n)
+{
+    if (n==1 || n==2){ return 1;}
+    return fibbo(n-1)+fibbo(n-2);
+}
+int main()
+{
+    int n;
+    cout<<"Enter a number ";
+    cin>>n;
+    cout<<fibbo(n);
+}
